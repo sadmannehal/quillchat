e7b859b2a46413aba3fa8ae9392eb17f9431ede8
sadmannehal <nehaledge@gmail.com>
2023-02-05 21:14:14 +0600

    add: middleware in util with checkAuth

---
+++ server/utils/middleware.js
@@ -0,0 +1,23 @@
+const jwt = require("jsonwebtoken");
+const userModel = require("../models/user-model");
+
+class Middleware {
+    static checkAuth (req, res, next) {
+        const auth = req.headers.authorization;
+        const token = auth && auth.split("Bearer ")[1];
+
+        if (!token) return res.status(401).json({ message: "Unauthorized" });
+        jwt.verify(token, process.env.JWT_TOKEN_SECRET, (err, data) => {
+            if (err) return res.status(403).json({ message: "Invalid token" });
+
+            userModel.findOne(data._id, (err, user) => {
+                if (err) return res.status(500).json({ message: "Internal server error" });
+
+                req.user = user;
+                next();
+            });
+        });
+    }
+}
+
+module.exports = Middleware;
